33a8b3b1b5e07cba984c006a04c73f774da5f812
samshereda <70921492+samshereda@users.noreply.github.com>
2021-01-08 16:57:55 -0500

    changed app according to revision request

--- scripts.js
+++ scripts.js
@@ -1,119 +1,173 @@
 /* eslint-disable indent */
 // eslint-disable-next-line strict
 $(function(){
-    let quizStarted = false;
-    let score = 0;
-    let questionNumber = 0;
-
-    const QUESTIONS = [
-        {
-            question: 'How many eyes do bees have?',
-            answers: ['2','5','6','8'],
-            correctAnswer: 1
-        },
-        {
-            question: 'What is the only marsupial native to North America?',
-            answers: ['Raccoon','Beaver','Black-Footed Ferret','Opossum'],
-            correctAnswer: 3
-        },
-        {
-            question: 'Which "bear" is not a true bear?',
-            answers: ['Koala Bear','Spectacled Bear','Giant Panda','Sloth Bear'],
-            correctAnswer: 0
-        },
-        {
-            question: 'What is the largest fish?',
-            answers: ['Ocean Sunfish','Blue Whale','Whale Shark','Great White Shark'],
-            correctAnswer: 2
-        },
-        {
-            question: 'What is NOT TRUE about sloths?',
-            answers: ['Sloths sleep up to 18 hours a day','Sloths can hold their breath for up to 40 minutes','Sloths will sometimes eat bird eggs','Sloths spend their entire lives hanging from trees'],
-            correctAnswer: 3
-        }
-    ];
+    const STORE = {
+        quizStarted: false,
+        questionNumber: 0,
+        questions:[
+            {
+                question: 'How many eyes do bees have?',
+                answers: ['2','5','6','8'],
+                correctAnswer: 1,
+                playerAnswer: null
+            },
+            {
+                question: 'What is the only marsupial native to North America?',
+                answers: ['Raccoon','Beaver','Black-Footed Ferret','Opossum'],
+                correctAnswer: 3,
+                playerAnswer: null
+            },
+            {
+                question: 'Which "bear" is not a true bear?',
+                answers: ['Koala Bear','Spectacled Bear','Giant Panda','Sloth Bear'],
+                correctAnswer: 0,
+                playerAnswer: null
+            },
+            {
+                question: 'What is the largest fish?',
+                answers: ['Ocean Sunfish','Blue Whale','Whale Shark','Great White Shark'],
+                correctAnswer: 2,
+                playerAnswer: null
+            },
+            {
+                question: 'What is NOT TRUE about sloths?',
+                answers: ['Sloths sleep up to 18 hours a day','Sloths can hold their breath for up to 40 minutes','Sloths will sometimes eat bird eggs','Sloths spend their entire lives hanging from trees'],
+                correctAnswer: 3,
+                playerAnswer: null
+            }
+        ]
+    };
 
     function render(){
         //render the quiz app
-        console.log('`render` ran')
-        if (quizStarted){
-            generateQuestion(questionNumber);
+        console.log('`render` ran');
+        
+        if (STORE.quizStarted){
+            console.log(STORE);
+            if(STORE.questionNumber >= STORE.questions.length){
+                console.log('quiz finished');
+                $('main').html(generateFinalPage());
+            } else if (STORE.questions[STORE.questionNumber].playerAnswer !== null){
+                const question = STORE.questions[STORE.questionNumber];
+                console.log(question);
+                $('main').html(generateAnswerPage(question.correctAnswer === question.playerAnswer));
+            } else {
+                $('main').html(generateQuestionPage());
+            }
         } else {
-            $('.quiz').html(`<h2>Ready to start?</h2>
-                <button class="button" id='start-quiz'><span>Start!</span></button>`);
-            startQuiz();
-
+            $('main').html('<h1>Welcome to the Animal Quiz.</h1>'+generateStartPage());
         }
     }
 
-    function generateQuestion(){
+    function generateStartPage(){
+        return `<h2>Ready to start?</h2>
+        <button class="button" id='start-quiz'><span>Start!</span></button>`;
+    }
+
+    function generateQuestionPage(){
         //Generate an html string from the question
-        let question = QUESTIONS[questionNumber];
-        $('.quiz').html(`<h2>Question ${questionNumber+1}</h2>
+        let question = STORE.questions[STORE.questionNumber];
+        return `<h2>Question ${STORE.questionNumber+1}</h2>
         <h3>${question.question}</h3>
         <form class="question">
-        <input type="radio" id="${question.answers[0]}" name = "quiz" value = "${question.answers[0]}" required>
-        <label for="${question.answers[0]}">${question.answers[0]}</label><br>
-        <input type="radio" id="${question.answers[1]}" name = "quiz" value = "${question.answers[1]}">
-        <label for="${question.answers[1]}">${question.answers[1]}</label><br>
-        <input type="radio" id="${question.answers[2]}" name = "quiz" value = "${question.answers[2]}">
-        <label for="${question.answers[2]}">${question.answers[2]}</label><br>
-        <input type="radio" id="${question.answers[3]}" name = "quiz" value = "${question.answers[3]}">
-        <label for="${question.answers[3]}">${question.answers[3]}</label><br>
-        <input class="button" type="submit" value="Submit"></form>`)
-        handleAnswer(question);
+        <input type="radio" id="0" name = "answer" value = 0 required>
+        <label for="0">${question.answers[0]}</label><br>
+        <input type="radio" id="1" name = "answer" value = 1>
+        <label for="1">${question.answers[1]}</label><br>
+        <input type="radio" id="2" name = "answer" value = 2>
+        <label for="2">${question.answers[2]}</label><br>
+        <input type="radio" id="3" name = "answer" value = 3>
+        <label for="3">${question.answers[3]}</label><br>
+        <input class="button" type="submit" value="Submit"></form>`
     } 
 
     function generateAnswerPage(correct){
+        console.log(correct);
         //Generate an html string for the answer page
         let pageText;
+        let button;
         if (correct){
-            score++;
-            pageText = `<h2>Question ${questionNumber+1}</h2>
+            pageText = `<h2>Question ${STORE.questionNumber+1}</h2>
             <h3>Correct!</h3>
-            <p>The correct answer is: ${QUESTIONS[questionNumber].answers[QUESTIONS[questionNumber].correctAnswer]}</p>`;
+            <p>The correct answer is: ${STORE.questions[STORE.questionNumber].answers[STORE.questions[STORE.questionNumber].correctAnswer]}</p>`;
         } else {
-            pageText = `<h2>Question ${questionNumber+1}</h2>
+            pageText = `<h2>Question ${STORE.questionNumber+1}</h2>
             <h3>Incorrect.</h3>
-            <p>The correct answer is: ${QUESTIONS[questionNumber].answers[QUESTIONS[questionNumber].correctAnswer]}</p>`;
+            <p>The correct answer is: ${STORE.questions[STORE.questionNumber].answers[STORE.questions[STORE.questionNumber].correctAnswer]}</p>`;
         }
 
-        if(questionNumber === QUESTIONS.length-1){
-            $('.quiz').html(pageText+`<p>You have completed the quiz. You answered ${score} out of ${QUESTIONS.length} questions correctly.</p>`)
+        if (STORE.questionNumber>=STORE.questions.length){
+            button = 'Next question';
         } else {
-            $('.quiz').html(pageText+'<button class="button next-question"><span>Next question</span></button>')
-            questionNumber++;
-            nextQuestion();
+            button = 'Finish';
         }
+
+        return `${pageText}
+        <p>You've answered ${getScore()} out of ${STORE.questionNumber + 1} questions correcly.</p>
+        <button class="button next-question">
+            <span>${button}</span>
+        </button>`;
         
     }
 
-    function handleAnswer(q){
-        //Take answer input and add to score
-        $('.question').submit(function(event) {
-            event.preventDefault();
-            console.log('`handleAnswer` ran');
-            generateAnswerPage(document.getElementById(q.answers[q.correctAnswer]).checked);
-        });
+    function generateFinalPage(){
+        return `<p>You have completed the quiz. You answered ${getScore()} out of ${STORE.questions.length} questions correctly.</p>
+            <button class="button restart-quiz"><span>Restart</span></button>`;
     }
 
-    function startQuiz(){
-        //Start quiz when button is pressed
-        $('.quiz').on('click','#start-quiz', event => {
-            console.log('`startQuiz` ran');
-            quizStarted = true;
+    function getScore(){
+        let score = 0;
+        STORE.questions.forEach(q => {
+            if (q.playerAnswer === q.correctAnswer){
+                score++;
+            }
+        });
+        return score;
+    }
+
+    function handleAnswer(){
+        //Take answer input and add to score
+        $('.quiz').on('submit','.question', event => {
+            event.preventDefault();
+            console.log('`handleAnswer` ran');
+            const answer = parseInt($('input[name=answer]:checked','.question').val());
+            STORE.questions[STORE.questionNumber].playerAnswer = answer;
             render();
         });
     }
 
-    function nextQuestion(){
+    function handleStartQuiz(){
+        //Start quiz when button is pressed
+        $('.quiz').on('click','#start-quiz', event => {
+            STORE.quizStarted = true;
+            render();
+        });
+    }
+
+    function handleNextQuestion(){
         //Show the next question after going to the answer page
-        console.log('`nextQuestion` ran')
         $('.quiz').on('click','.next-question', event => {
+            STORE.questionNumber++;
+            render();
+        });
+    }
+
+    function handleRestart(){
+        $('.quiz').on('click','.restart-quiz', event => {
+            console.log('restart ran');
+            STORE.quizStarted=false;
+            STORE.questionNumber = 0;
+            STORE.questions.forEach(q => {
+                q.playerAnswer = null;
+            });
             render();
         });
     }
 
     render();
+    handleStartQuiz();
+    handleAnswer();
+    handleNextQuestion();
+    handleRestart();
     
 });
